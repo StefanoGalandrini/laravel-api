9d9aba95eaabf0e94ddc74e0fc62745a83b6abc1
StefanoGalandrini <stefe68@gmail.com>
2023-07-14 16:28:27 +0200

    Add changes to delete account, still not working

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,49 +1,42 @@
 <section class="space-y-6">
 	<header>
-		<h2>
-			Delete Account
-		</h2>
-
-		<p>
-			Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your
-			account, please download any data or information that you wish to retain.
-		</p>
+		<h2>Delete Account</h2>
+		<p>Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your
+			account, please download any data or information that you wish to retain.</p>
 	</header>
 
-	<form method="post" action="{{ route('admin.profile.destroy') }}">
-		@csrf
-		@method('delete')
-
-		<div class="mt-2">
-			<label for="password_delete">Password</label>
-			<input id="password_delete" name="password" type="password">
-			@error('password')
-				{{ $message }}
-			@enderror
-		</div>
-
-		<button type="button" class="btn btn-danger mt-2" data-toggle="modal" data-target="#deleteAccount">Delete
-			Account</button>
-	</form>
+	<button type="button" class="btn btn-danger mt-2" data-toggle="modal" data-target="#deleteAccount">Delete Account</button>
 </section>
 
-{{-- Modal --}}
-<div class="modal fade" id="deleteAccount" tabindex="-1" role="dialog">
+<div class="modal fade" id="deleteAccount" tabindex="-1" role="dialog" aria-labelledby="deleteAccountLabel"
+	aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="deleteAccountLabel">Modal title</h5>
-				<button type="button" class="close" data-dismiss="modal">
+				<h5 class="modal-title" id="deleteAccountLabel">Are you sure?</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-			<div class="modal-body">
-				This Account will be deleted: are you sure?
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-danger">Delete</button>
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			</div>
+			<form method="POST" action="{{ route('admin.profile.destroy') }}">
+				@csrf
+				@method('delete')
+				<div class="modal-body">
+					<p>Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your
+						password to confirm.</p>
+					<div class="form-group">
+						<label for="password_delete">Password</label>
+						<input id="password_delete" name="password" type="password" class="form-control">
+						@error('password')
+							<small class="text-danger">{{ $message }}</small>
+						@enderror
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-danger">Delete</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
